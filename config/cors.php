ccb6c86e3e32147fea245001b715804795324d5f
devjmmg <166524587+devjmmg@users.noreply.github.com>
2026-08-12 00:41:32 -0600

    feat: remove files

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'https://react-kiosk-jmmg.netlify.app',
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
@@ -31,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
